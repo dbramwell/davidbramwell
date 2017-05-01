ad867a456224e1bd4a9073a26a53c71ad58ea052
bram <david.j.bramwell@googlemail.com>
2017-05-01 06:34:44 -0300

    Completed emailer

--- spec/services/emailService/lib/emailer.spec.js
+++ spec/services/emailService/lib/emailer.spec.js
@@ -1,21 +1,61 @@
+var nodemailer = require('nodemailer');
+var mockTransport = require('nodemailer-mock-transport');
+
 describe("emailer", function() {
   var Emailer = require('../../../../src/services/emailService/lib/emailer');
 
   beforeEach(function() {
     emailer = new Emailer();
-    process.env.EMAIL = 'test@test.com';
+    name = "David";
+    fromEmail = "david@bramwell.co.uk";
+    toEmail = "to@email.com";
+    content = "This is my email content";
+    process.env.MAILGUN_EMAIL = "mailgun@email.com";
+    process.env.MAILGUN_PASS = "mailgun_password";
   });
 
   it("should be able to generate correct email content", function() {
-    const name = "David";
-    const email = "david@bramwell.co.uk";
-    const content = "This is my email content";
-    const emailContent = emailer.generateContent(name, email, content);
+    const emailContent = emailer.generateContent(name, fromEmail, content);
     expect(emailContent).toEqual('<h2>Name:</h2>' +
-    '<p>' + name + '</p><br>' +
-    '<h2>Email:</h2>' +
-    '<p>' + email + '</p><br>' +
-    '<h2>Content:</h2>' +
-    '<p>' + content + '</p>');
+      '<p>' + name + '</p><br>' +
+      '<h2>Email:</h2>' +
+      '<p>' + fromEmail + '</p><br>' +
+      '<h2>Content:</h2>' +
+      '<p>' + content + '</p>');
+  });
+
+  it("getMailOptions should return the correct map of mail options", function() {
+    const options = emailer.getMailOptions(name, fromEmail, toEmail, content);
+    expect(options).toEqual({
+      from: fromEmail,
+      to: toEmail,
+      subject: 'Website contact from ' + fromEmail,
+      html: emailer.generateContent(name, fromEmail, content)
+    });
+  });
+
+  it("getTransporter returns nodemailer transport for mailgun service", function() {
+    const transporter = emailer.getTransporter();
+    expect(transporter.options.service).toEqual('Mailgun');
+    expect(transporter.options.auth.user).toEqual(process.env.MAILGUN_EMAIL);
+    expect(transporter.options.auth.pass).toEqual(process.env.MAILGUN_PASS);
+  });
+
+  it('sendEmail should send correct mail', function() {
+    const transport = mockTransport();
+    const transporter = nodemailer.createTransport(transport);
+    spyOn(emailer, "getTransporter").and.returnValue(transporter);
+    emailer.sendEmail(name, fromEmail, toEmail, content);
+    expect(emailer.getTransporter).toHaveBeenCalled();
+    expect(transport.sentMail.length).toEqual(1);
+    expect(transport.sentMail[0].data.to).toEqual(toEmail);
+    expect(transport.sentMail[0].data.from).toEqual(fromEmail);
+    expect(transport.sentMail[0].message.content).toEqual(
+      '<h2>Name:</h2>' +
+      '<p>' + name + '</p><br>' + 
+      '<h2>Email:</h2>' + 
+      '<p>' + fromEmail + '</p><br>' +
+      '<h2>Content:</h2><p>' + content + '</p>');
+    expect(transport.sentMail[0].data.subject).toEqual('Website contact from ' + fromEmail);
   });
 });
--- src/services/emailService/lib/emailer.js
+++ src/services/emailService/lib/emailer.js
@@ -1,3 +1,5 @@
+var nodemailer = require('nodemailer');
+
 function Emailer() {}
 
 function generateContent(name, email, content) {
@@ -9,8 +11,33 @@ function generateContent(name, email, content) {
 		'<p>' + content + '</p>'
 }
 
+function getMailOptions(name, fromEmail, toEmail, content) {
+	return {
+		from: fromEmail,
+		to: toEmail,
+		subject: 'Website contact from ' + fromEmail,
+		html: generateContent(name, fromEmail, content)
+	};
+}
+
+function getTransporter() {
+	return nodemailer.createTransport({
+		service: 'Mailgun',
+		auth: {
+			user: process.env.MAILGUN_EMAIL,
+			pass: process.env.MAILGUN_PASS
+		}
+	});
+}
+
+Emailer.prototype.sendEmail = function(name, fromEmail, toEmail, content) {
+	this.getTransporter().sendMail(getMailOptions(name, fromEmail, toEmail, content));
+};
+
 if (process.env.ENVIRONMENT === "test") {
-	Emailer.prototype.generateContent = generateContent
+	Emailer.prototype.generateContent = generateContent;
+	Emailer.prototype.getMailOptions = getMailOptions;
+	Emailer.prototype.getTransporter = getTransporter;
 }
 
 module.exports = Emailer;
